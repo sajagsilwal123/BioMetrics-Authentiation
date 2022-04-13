14d92d76713afcf2338b0fd9e78412291734dbed
sajagsilwal123 <sajag.silwal123@gmail.com>
2022-04-13 23:52:36 +0545

    .

--- lib/main.dart
+++ lib/main.dart
@@ -1,21 +1,67 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:local_auth/local_auth.dart';
 
 void main (){
   runApp(
-    MyApp()
+    MaterialApp(
+      home: Authenticator()
+    )
   );
 }
 
-class MyApp extends StatefulWidget {
-  const MyApp({Key? key}) : super(key: key);
+class Authenticator extends StatefulWidget {
+  const Authenticator({Key? key}) : super(key: key);
 
   @override
-  State<MyApp> createState() => _MyAppState();
+  State<Authenticator> createState() => _AuthenticatorState();
 }
 
-class _MyAppState extends State<MyApp> {
+class _AuthenticatorState extends State<Authenticator> {
+
+  bool _canCheckBiometrics = false;
+  List<BiometricType> _availableBiometric =[];
+  String _authorized = "Not Authorized";
+
+  Future<void> _checkBiometrics() async {
+  }
+
+  Future<void> _getAvailableBiometric() async {
+  }
+
+  Future<void> _authenticate() async {
+
+  }
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('FingerPrint Authenticator'),
+      ),
+      body: Center(
+        child: Container(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              Text('Can Check biometrics: $_canCheckBiometrics'),
+              ElevatedButton(
+                child: Text('Check Biometrics'),
+                onPressed: _checkBiometrics ,
+              ),
+              Text('Available Biometric: $_availableBiometric'),
+              ElevatedButton(
+                child: Text('get available biometrics'),
+                onPressed: _getAvailableBiometric,
+              ),
+              Text('Current State: \n'),
+              ElevatedButton(
+                child: Text("Authenticate"),
+                onPressed: _authenticate,
+              )
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
